ed36031d8dd7240c404e2486be2473f4558810f3
FacundoBeldi <facu.beldi@hotmail.com>
2024-04-30 16:25:20 -0300

    segunda clase

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ document.getElementById('formulario-id').addEventListener('submit', function(eve
 });
 */
 
-const usuarios = [
+const users = [
     {
         fullname: 'John Doe',
         age: 30,
@@ -83,7 +83,7 @@ const usuarios = [
         password: 'admin',
         bornDate: 725846400000,
         location: 'La Lucila',
-        image: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/mario-kart-for-wii-u/7/71/Mk8iconyoshi.png?width=1280',
+        image: 'https://randomuser.me/api/portraits/men/68.jpg',
         role: 'ADMIN_ROLE'
     },
     {
@@ -95,7 +95,7 @@ const usuarios = [
         password: 'user',
         bornDate: '1999-06-15',
         location: 'Escobar',
-        image: 'https://randomuser.me/api/portraits',
+        image: 'https://randomuser.me/api/portraits/men/69.jpg',
         role: 'user'
     },
     {
@@ -107,7 +107,7 @@ const usuarios = [
         password: 'user',
         bornDate: '1999-05-19',
         location: 'Garin',
-        image: 'https://randomuser.me/api/portraits',
+        image: 'https://randomuser.me/api/portraits/men/70.jpg',
         role: 'user'
     },
     {
@@ -117,16 +117,64 @@ const usuarios = [
         id: '4',
         active: true,
         password: 'user',
-        bornDate: '1991-05-19',
+        bornDate: '1996-05-19',
         location: 'Maschwitz',
-        image: 'https://randomuser.me/api/portraits',
+        image: 'https://randomuser.me/api/portraits/men/71.jpg',
+        role: 'user'
+    },
+    {
+        fullname: 'Ricardo Beldi',
+        age: 61,
+        email: 'ricardo@gmail.com',
+        id: '5',
+        active: false,
+        password: 'user',
+        bornDate: '1963-08-24',
+        location: 'San Martin',
+        image: 'https://randomuser.me/api/portraits/men/72.jpg',
+        role: 'user'
+    },
+    {
+        fullname: 'Santiago Beldi',
+        age: 28,
+        email: 'santi@gmail.com',
+        id: '6',
+        active: true,
+        password: 'user',
+        bornDate: '1996-07-24',
+        location: 'Maschwitz',
+        image: 'https://randomuser.me/api/portraits/men/73.jpg',
+        role: 'user'
+    },
+    {
+        fullname: 'Tomas Martinez',
+        age: 24,
+        email: 'tomi@gmail.com',
+        id: '7',
+        active: true,
+        password: 'user',
+        bornDate: '1999-02-19',
+        location: 'Escobar',
+        image: 'https://randomuser.me/api/portraits/men/74.jpg',
+        role: 'user'
+    },
+    {
+        fullname: 'Julian Caceres',
+        age: 29,
+        email: 'juli@gmail.com',
+        id: '8',
+        active: true,
+        password: 'user',
+        bornDate: '1995-03-01',
+        location: 'Pilar',
+        image: 'https://randomuser.me/api/portraits/men/75.jpg',
         role: 'user'
     },
 ];
 
 
-
-usuarios.forEach(function (usuario, indice) {
+/*
+usuarios.forEach((users) => {
     // console.log(prod, indice)
     console.log(usuario['fullname'])
 
@@ -138,15 +186,16 @@ usuarios.forEach(function (usuario, indice) {
         numeracion = indice;
     }
 
-    document.write("${numeracion} - ${prod.nombre} al precio $${prod.precio} <br>")
+    document.write(`${numeracion} - ${prod.nombre} al precio $${prod.precio} <br>`)
 })
+*/
 
 //MAPEO --> A diferencia del for, que recorres el array, acá hacer un array nuevo
 
-const usuariosFinal = usuarios.map(function (usuario, indice) {
+const usuariosFinal = users.map((users) => {
     const ususariosModificado = {
-        fullname: usuarios.fullname.toUpperCase(),
-        email: usuarios.email,
+        fullname: users.fullname,
+        email: users.email,
     }
     return ususariosModificado
 })
@@ -155,8 +204,8 @@ console.log(usuariosFinal)
 
 //Filter
 
-const usuariosFiltrados = usuarios.filter(function (usuario, indice) {
-    if (usuario.active) {
+const usuariosFiltrados = users.filter((users) => {
+    if (users.active) {
         return true
     }
     else {
@@ -164,11 +213,11 @@ const usuariosFiltrados = usuarios.filter(function (usuario, indice) {
     }
 })
 
-console.log(productosFiltrados)
+console.log(usuariosFiltrados)
 
 //Find
 //Una iteración que cumple 1 solo. Es decir, va a mostrar el primer Adidas, no todos.
-const usuarioEncontrado = usuarios.find((user) => {
+const usuarioEncontrado = users.find((user) => {
     if (user.id === '1') {
         return true
     } else {
@@ -177,3 +226,109 @@ const usuarioEncontrado = usuarios.find((user) => {
 })
 
 console.log(usuarioEncontrado)
+
+//Rellenar la tabla
+
+const tableHTML = document.getElementById("table-container");
+
+const tableBodyHTML = document.getElementById("table-body");
+
+function renderUsers(arrayUsers) {
+
+    tableBodyHTML.innerHTML = '';
+
+    arrayUsers.forEach((user) => {
+        tableBodyHTML.innerHTML += `
+    <tr>
+        <td class="user-image">
+            <img src="${user.image}" alt ="${user.fullname} avatar">
+        </td>
+        <td class="user-name"> ${user.fullname}</td>
+        <td class="user-mail"> ${user.email}</td>
+        <td class="user-city"> ${user.location}</td>
+        <td class="user-age"> ${user.age}</td>
+        <td class="user-active> ${user.active}</td>
+        <td class="cont-btn">
+        <button class="btn btn-danger btn-sm" onclick="deleteUser('${user.id}')">
+            <i class="fa-solid fa-trash"></i>
+        </button>
+        <button class="btn btn-primary btn-sm" onclick="deleteUser('${user.id}')">
+            <i class="fa-solid fa-pencil"></i>
+        </button>
+        </td>
+    </tr>
+    `
+    })
+}
+
+renderUsers(users);
+
+//Busqueda de usuarios
+
+function inputSearch(evt) {
+    console.log(evt.target.value)
+
+    const search = evt.target.value.toLowerCase();
+
+    const filteredUsers = users.filter((usr) => {
+        if (usr.fullname.toLocaleLowerCase().includes(search)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    })
+    renderUsers(filteredUsers);
+}
+
+//Ordenamiento
+
+function sortDesc(){
+    users.sort((a, b) =>{
+        if(a.fullname.toLocaleLowerCase()<b.fullname.toLocaleLowerCase()){
+            return 1;
+        }
+        if(a.fullname.toLocaleLowerCase()>b.fullname.toLocaleLowerCase()){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    })
+
+    renderUsers(users);
+}
+
+function sortAsc(){
+    users.sort((a, b) =>{
+        if(a.fullname.toLocaleLowerCase()>b.fullname.toLocaleLowerCase()){
+            return 1;
+        }
+        if(a.fullname.toLocaleLowerCase()<b.fullname.toLocaleLowerCase()){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    })
+
+    renderUsers(users);
+}
+
+//Eliminar Usuario
+
+function deleteUser(idUser){
+    const indice = users.findIndex((usr)=>{ //obtengo el indice
+        if  (usr.id === idUser){ //me fijo si el indice coincide con el id del usuario
+            return true; //verdadero si coincide
+        }
+    })
+    if(indice === -1){ //si no coincide, error
+        SwalFire({title: "Error al borrar", text:"No se pudo encontrar el usuario", icon: "error"})
+        return;
+    }
+
+    users.splice(indice, 1) //elimina
+
+    renderUsers(users) //vuelvo a pintar la tabla
+}
